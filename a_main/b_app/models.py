3cbd7f736b7d6b10a94fc2a5f8422964c042472d
Ahmad Qamar <ahmadqamar.rq@gmail.com>
2026-04-29 06:21:29 +0500

    .

--- a_main/b_app/models.py
+++ a_main/b_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    first_name=models.CharField(max_length=100)
+    last_name=models.CharField(max_length=100)
+    email=models.EmailField(unique=True)
+    address=models.TextField()
+
+def __str__(self):
+    return self.name
+
